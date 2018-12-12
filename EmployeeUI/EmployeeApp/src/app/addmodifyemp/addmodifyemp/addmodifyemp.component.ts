8a53fca8f94b713d6e127baed8b44e630259a87c
Supraja <Supraja@SathishMaha-PC>
2018-12-12 09:38:51 +0530

    sorting selection and modal

--- EmployeeUI/EmployeeApp/src/app/addmodifyemp/addmodifyemp/addmodifyemp.component.ts
+++ EmployeeUI/EmployeeApp/src/app/addmodifyemp/addmodifyemp/addmodifyemp.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Student} from './../../model/student.model';
+import {DatagridPropertyComparator,ClrDatagridSortOrder} from '@clr/angular';
+import {ClrDatagridPagination,ClrDatagrid} from '@clr/angular';
 
 @Component({
   selector: 'app-addmodifyemp',
@@ -6,10 +9,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./addmodifyemp.component.css']
 })
 export class AddmodifyempComponent implements OnInit {
+  addNewPopup=false;
+  selected: string[] = [];
+  nameSortOrder =ClrDatagridSortOrder.DESC;
+  nameComparator = new DatagridPropertyComparator('name');
+  idSortOrder =ClrDatagridSortOrder.DESC;
+  idComparator = new DatagridPropertyComparator('id');
+  students:Student[]=[
+    { id:1, name: 'Supraja', passport: 'xyz', isactive: 'Y', status: 'approved', history_id: 0 },
+    { id:2, name: 'girisha', passport: 'abc', isactive: 'Y', status: 'approved', history_id: 0 },
+    { id:3, name: 'Chandra', passport: 'abc', isactive: 'Y', status: 'approved', history_id: 0 },
+    { id:4, name: 'Suresh', passport: 'abc', isactive: 'Y', status: 'approved', history_id: 0 },
+    { id:5, name: 'Lalitha', passport: 'abc', isactive: 'Y', status: 'approved', history_id: 0 },
+    { id:6, name: 'Ram', passport: 'abc', isactive: 'Y', status: 'approved', history_id: 0 },
+    { id:7, name: 'Latha', passport: 'abc', isactive: 'Y', status: 'approved', history_id: 0 },
+    { id:8, name: 'Vijay', passport: 'abc', isactive: 'Y', status: 'approved', history_id: 0 },
+    { id:9, name: 'Adhira', passport: 'abc', isactive: 'Y', status: 'approved', history_id: 0 },
+    { id:10, name: 'Ashwin', passport: 'abc', isactive: 'Y', status: 'approved', history_id: 0 }
+  ];
 
   constructor() { }
 
   ngOnInit() {
   }
-
+  
+  onAdd() {
+    this.addNewPopup =true;
+  } 
 }
---
+++ EmployeeUI/EmployeeApp/src/app/model/student.model.ts
@@ -0,0 +1,10 @@
+export class Student {
+    id: number;
+    name: string;
+    passport: string;
+    isactive: string;
+    status: string;
+    history_id: number;
+}
+
+
